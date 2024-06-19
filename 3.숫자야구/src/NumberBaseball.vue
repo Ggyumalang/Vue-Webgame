d3b2de67a39646accbfe9203dc4caffe49c025a1
Ggyumalang <khg2154@naver.com>
2024-06-19 23:21:31 +0900

    2024.06.19 숫자야구 만들기

--- 3.숫자야구/src/NumberBaseball.vue
+++ 3.숫자야구/src/NumberBaseball.vue
@@ -1,40 +1,47 @@
 <template>
     <div>
         <h1>{{ result }}</h1>
-        <form @submit="onSubmitForm">
+        <form @submit.prevent="onSubmitForm">
             <input maxlength="4" ref="inputVal" v-model="value" />
             <button>입력</button>
         </form>
-        <div> 시도 : {{ tryNumber }}</div>
-        <div> {{ strikeNumber }} 스트라이크 {{ ballNumber }} 볼입니다.</div>
+        <div> 시도 : {{ tries.length }}</div>
+        <ul>
+            <li v-for="t in tries">
+                <div>{{ t.try }}</div>
+                <div>{{ t.result }}</div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
+
+//일반 메서드.. 
+const generateAnswer = () => {
+    let digits = [];
+    while (digits.length < 4) {
+        const digit = Math.ceil(Math.random() * 9).toString();
+        if (!digits.includes(digit)) {
+            digits.push(digit);
+        }
+    }
+    return digits.join(''); // 숫자가 아닌 문자열을 반환
+}
+
 export default ({
     data() {
         return {
             value : '',
             result : '',
-            answer: this.generateAnswer(),
-            tryNumber : 0,
-            strikeNumber : 0,
-            ballNumber : 0,
+            answer: generateAnswer(),
+            tries : []
         }
     },
     methods : {
-        generateAnswer() {
-            let digits = [];
-            while (digits.length < 4) {
-                const digit = Math.ceil(Math.random() * 9).toString();
-                if (!digits.includes(digit)) {
-                    digits.push(digit);
-                }
-            }
-            return digits.join(''); // 숫자가 아닌 문자열을 반환
-        },
+        onSubmitForm() {
 
-        onSubmitForm(e) {
-            e.preventDefault();
+            console.log(this.answer);
+ 
             if (this.value.length !== 4) {
                 this.result = '4자리 숫자를 입력해주세요.';
                 return;
@@ -43,40 +50,48 @@ export default ({
                 this.result = '숫자만 입력해주세요.';
                 return;
             }
-            this.checkAnswer();
-            this.tryNumber++;
 
-            if (this.strikeNumber === 4) {
-                this.result = `정답입니다! ${this.tryNumber}번 만에 맞추셨습니다.`;
-                this.answer = this.generateAnswer();
-                this.tryNumber = 0;
+            if (this.value === this.answer) {
+                this.result = `정답입니다! ${this.tries.length + 1}번 만에 맞추셨습니다.`;
+                this.answer = generateAnswer();
+                this.tries.push({
+                    try : this.value,
+                    result : '홈런',
+                });
+                alert('게임을 다시 시작합니다.');
+                this.tries = [];
             } else {
-                this.result = `${this.strikeNumber} 스트라이크, ${this.ballNumber} 볼입니다.`;
-            }
-            this.value = '';
-        },
 
-        checkAnswer() {
-            console.log(this.answer);
-            this.strikeNumber = 0;
-            this.ballNumber = 0;
+                if(this.tries.length >= 9) {
+                    this.result = `10번 넘게 틀려서 실패! 답은 ${this.answer}입니다!`;
+                    alert('게임을 다시 시작합니다.');
+                    this.tries = [];
+                    this.value = '';
+                    this.answer = generateAnswer(); 
+                    this.$refs.inputVal.focus();
+                } else {
+                    let strike = 0;
+                    let ball = 0;
 
-            const answerArray = this.answer.split('');
-            const valueArray = this.value.split('');
-
-            //스트라이크 및 볼 개수 계산하기
-            for(let i = 0; i < 4; i++) {
-                
-                console.log("valueArray >>> ", valueArray[i]);
-                console.log("answerArray >>> ", answerArray[i]);
-
-                if(valueArray[i] === answerArray[i]) {
-                    this.strikeNumber++;
-                } else if(answerArray.includes(valueArray[i])) {
-                    this.ballNumber++;
+                    const answerArr = this.value.split('').map(v => parseInt(v));
+                    for(let i = 0 ; i < 4; i++) {
+                        if(answerArr[i] === this.answer[i]) {
+                            strike++;
+                        } else if(this.answer.includes(answerArr[i])) {
+                            ball++;
+                        }
+                    }
+                    this.result = `실패입니다.`;
+                    this.tries.push({
+                        try : this.value,
+                        result : `${strike} 스트라이크 ${ball} 볼 입니다.`,
+                    });
                 }
             }
-        }
+            
+            this.$refs.inputVal.focus();
+            this.value = ''; 
+        },
     }
 })
 </script>
